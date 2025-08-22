31983a8c92cb188ec1982e606fc55180cba73d0d
Sandeep Raina <127074583+Sandeepraina40@users.noreply.github.com>
2025-08-22 11:49:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const makeChange = (c) => {
-  // your name here
+  // convert input to number (prompt gives string)
+  c = Number(c);
+
+  // calculate coins
+  const q = Math.floor(c / 25);
+  c = c % 25;
+
+  const d = Math.floor(c / 10);
+  c = c % 10;
+
+  const n = Math.floor(c / 5);
+  c = c % 5;
+
+  const p = c;
+
+  // return object
+  return { q, d, n, p };
 };
 
-// Do not the change the code below
+// Do not change the code below
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
